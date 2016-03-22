fac37247424abbd2460c376f5e82c07314c66067
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-03-22 01:41:22 -0700

    refactored

--- controllers.js
+++ controllers.js
@@ -1,19 +1,21 @@
 // controllers
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService ){
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService ){
   $scope.city  = cityService.city;
 
   $scope.$watch('city', function(){
     cityService.city  = $scope.city;
   });
+
+  $scope.submit = function(){
+    $location.path('/forecast');
+  };
 }]);
 
-weatherApp.controller('forecastController', ['$scope','$resource','$routeParams','cityService', function($scope, $resource, $routeParams, cityService){
+weatherApp.controller('forecastController', ['$scope','$routeParams','cityService','weatherService', function($scope, $routeParams, cityService, weatherService){
   $scope.city  = cityService.city;
   $scope.days = $routeParams.days || "2";
-  //http://api.openweathermap.org/data/2.5/forecast/daily?q=dallas&cnt=2&appid=6b154f5e9befc6a9b36dcc7047816782
-  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{
-    callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
-  $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt : $scope.days, appid: "6b154f5e9befc6a9b36dcc7047816782"});
+
+  $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
 
   $scope.convertToFahrenheit = function(degK){
     return Math.round((1.8* (degK-273))+32);
--- services.js
+++ services.js
@@ -2,3 +2,13 @@
 weatherApp.service('cityService', function(){
   this.city  = "Laguna Hills, CA";
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource){
+  this.GetWeather = function(city, days){
+    var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{
+      callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+
+    return weatherAPI.get({q: city, cnt : days, appid: "6b154f5e9befc6a9b36dcc7047816782"});
+
+  };
+}]);
